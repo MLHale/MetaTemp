dfa9d3c7cb992553d50baf2e2bf752931f88eac1
clenk <serpentine_cougar@yahoo.com>
2016-04-06 16:04:15 -0700

    Add a progress bar to use as a thermometer in the main activity

--- app/src/main/java/com/example/student/metatemp/MainActivity.java
+++ app/src/main/java/com/example/student/metatemp/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ProgressBar progress = (ProgressBar) findViewById(R.id.progressBar);
+        progress.setScaleY(5f);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
