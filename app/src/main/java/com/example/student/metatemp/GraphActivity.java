41d89cdf0cf7f52041c09b4c569b5eed2a132bb9
clenk <serpentine_cougar@yahoo.com>
2016-04-07 10:10:11 -0700

    Make Settings use a single category

--- app/src/main/java/com/example/student/metatemp/GraphActivity.java
+++ app/src/main/java/com/example/student/metatemp/GraphActivity.java
@@ -2,14 +2,11 @@ package com.example.student.metatemp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
