84781a3457ceba7f3acb1e3c00e15bebfd6860e2
clenk <serpentine_cougar@yahoo.com>
2016-04-27 16:40:48 -0700

    Add high/low threshold settings

--- app/src/main/java/com/example/student/metatemp/SettingsActivity.java
+++ app/src/main/java/com/example/student/metatemp/SettingsActivity.java
@@ -4,18 +4,22 @@ package com.example.student.metatemp;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
+import android.preference.SwitchPreference;
 import android.support.design.widget.AppBarLayout;
 import android.support.v4.app.NavUtils;
 import android.support.v4.content.ContextCompat;
@@ -31,6 +35,7 @@ import android.view.WindowManager;
 import android.widget.LinearLayout;
 
 import java.util.List;
+import java.util.prefs.Preferences;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -43,7 +48,8 @@ import java.util.List;
  * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
  * API Guide</a> for more information on developing a Settings UI.
  */
-public class SettingsActivity extends AppCompatPreferenceActivity {
+public class SettingsActivity extends AppCompatPreferenceActivity implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,6 +89,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 
         // Add preference options
         addPreferencesFromResource(R.xml.pref_fragment);
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
 
     /**
@@ -125,6 +132,41 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); ++i) {
+            Preference preference = getPreferenceScreen().getPreference(i);
+            if (preference instanceof PreferenceGroup) {
+                PreferenceGroup preferenceGroup = (PreferenceGroup) preference;
+                for (int j = 0; j < preferenceGroup.getPreferenceCount(); ++j) {
+                    Preference singlePref = preferenceGroup.getPreference(j);
+                    updatePreference(singlePref, singlePref.getKey());
+                }
+            } else {
+                updatePreference(preference, preference.getKey());
+            }
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        updatePreference(findPreference(key), key);
+    }
+
+    private void updatePreference(Preference preference, String key) {
+        if (preference == null) return;
+        else if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            listPreference.setSummary(listPreference.getEntry());
+            return;
+        }
+        else if (preference instanceof EditTextPreference) {
+            SharedPreferences sharedPrefs = getPreferenceManager().getSharedPreferences();
+            preference.setSummary(sharedPrefs.getString(key, "Default"));
         }
     }
 
